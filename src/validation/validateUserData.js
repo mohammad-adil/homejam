5882861046deab0784cee518a379d6f09b7fb80f
mohammad-adil <shahaadil13@gmail.com>
2021-06-18 15:10:26 +0530

    validation module push 2

--- src/validation/validateUserData.js
+++ src/validation/validateUserData.js
@@ -2,9 +2,15 @@ const validator = require('validator')
 
 
 const validate = async (req, res, next) => {
+    try {
 
-    let result = await checkUser(req.body)
+        let result = await checkUser(req.body)
+        console.log(result)
+    } catch (err) {
 
+        console.log(err)
+
+    }
 
 }
 
@@ -20,15 +26,25 @@ const checkUser = (data) => {
                 errors.phone = validator.isMobilePhone(data.phone);
             } catch (err) {
 
-                return reject("Email/Phone Number not Valid")
+                return reject('Internal Error')
             }
-
-            errors.data = data
-            return resolve(errors)
 
         } else {
             return reject('All Fields are required')
         }
+
+        if (errors.email == "false" && errors.phone == "false") {
+            console.log(errors.email + ' ' + errors.phone)
+            return reject('Email || Password Validation error')
+
+
+        } else {
+            console.log("all well ")
+        }
+
+
+        errors.data = data
+        return resolve(errors)
 
     })
 
